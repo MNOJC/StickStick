6f467b45aabfd8feab226a5f4c982279049dc87c
MNOJC <nathan.moulin718@gmail.com>
2024-02-19 06:43:16 +0100

    level name auto set

--- Assets/Scripts/LevelNumber.cs
+++ Assets/Scripts/LevelNumber.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LevelNumber : MonoBehaviour
 {
@@ -13,8 +14,15 @@ public class LevelNumber : MonoBehaviour
     [SerializeField]
     private MainCharacter player;
 
+    [SerializeField]
+    private TextMeshProUGUI name;
+
+    [SerializeField]
+    private LevelScoreData data;
+
     void Start()
     {
+        name.text = data.levelName;
         animator.SetTrigger("LevelName");
         Invoke("StartLevel",3f);
     }
