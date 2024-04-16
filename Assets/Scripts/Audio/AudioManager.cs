b45e9efe159e17154421326426b8ed8c09fadbd4
MNOJC <nathan.moulin718@gmail.com>
2024-04-16 04:34:09 +0200

    Adding audio

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -24,7 +24,6 @@ public class AudioManager : MonoBehaviour
 
     void Start()
     {
-        PlayMusic("Theme");
     }
     public void PlayMusic(string name)
     {
--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -8,13 +8,20 @@ public class Title : MonoBehaviour
     [SerializeField] private Animator animatorTitle;
 
     void Start()
-    {
+    { 
+        AudioManager.instance.PlaySFX("Title");
         animatorTitle.SetTrigger("Title");
         Invoke("OpenMenu", 12f);
+        Invoke("StartTheme", 4f);
     }
 
     void OpenMenu()
     {
         SceneManager.LoadScene("MenuScene", LoadSceneMode.Additive);
+    }
+
+    void StartTheme()
+    {
+        AudioManager.instance.PlayMusic("Theme");
     }
 }
